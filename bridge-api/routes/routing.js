73b7603b462a7033523c8fc07f46ac21bb66ec8b
Tyrone Jefferson <19tljefferson76@gmail.com>
2026-06-17 23:10:24 -0500

    Add dashboard link to manual hazards admin

--- bridge-api/routes/routing.js
+++ bridge-api/routes/routing.js
@@ -725,7 +725,7 @@ function renderManualHazardAdminPage(session = {}) {
     <a class="tab delivery" href="/api/delivery-notes/admin">Delivery Notes</a>
     <a class="tab" href="/api/route-manifests/admin">Route Manifests</a>
     <a class="tab" href="/api/drivers/admin">Driver Registry</a>
-    <a class="tab" href="/api/routing/manual-hazards/admin-users/admin">Admin Users</a>
+    <a class="tab" href="/api/admin">Supervisor Dashboard</a>
   </nav>
   <main>
     <div class="hero-strip">
